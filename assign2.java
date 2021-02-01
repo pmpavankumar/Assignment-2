3c0272047e77f54d5f807e6134ba2757bc6d0ce0
pmpavankumar <78338227+pmpavankumar@users.noreply.github.com>
2021-02-01 11:53:45 +0530

    Assignment-2
    This array assignment

---
+++ assign2.java
@@ -0,0 +1,22 @@
+package assignment2;
+
+public class assign2 {
+
+	    public static void main(String[] args) {
+	        int[] num = {1, 2, 3, 4, 5};
+	        int toFind = 6;
+	        boolean found = false;
+
+	        for (int n : num) {
+	            if (n == toFind) {
+	                found = true;
+	                break;
+	            }
+	        }
+
+	        if(found)
+	            System.out.println(toFind + " is found .");
+	        else
+	            System.out.println(" -1 ");
+	    }
+	}
